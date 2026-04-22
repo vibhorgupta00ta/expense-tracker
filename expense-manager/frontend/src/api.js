1f256ca00e3754ff304e61d349f751e1915ee0c0
vibhor <vibhorg62@gmail.com>
2026-04-22 17:33:57 +0530

    Fixed API base URL

--- expense-manager/frontend/src/api.js
+++ expense-manager/frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://expense-tracker-atss.onrender.com",
+  baseURL: "https://expense-tracker-atss.onrender.com/api",
 });
